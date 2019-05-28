572c757c1b4b72a94826dced9ef1025175270b91
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-28 23:10:52 +0000

    Automatically backed up by Learn

--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -37,6 +37,7 @@ describe "Operations" do
 
     it 'uses the ternary operator' do
       methods = file_contents.split("not_safe?")
+      binding.pry
       match = methods.last
       expect(match).to include("?")
     end
