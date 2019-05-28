a33c0bd6de3e91cd5fc8f4b473d63b2f4d7d620c
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-28 23:01:03 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -6,12 +6,12 @@ def unsafe?(speed)
   end
 end
 
-puts unsafe?(30)
-puts unsafe?(55)
-puts unsafe?(95)
+# puts unsafe?(30)
+# puts unsafe?(55)
+# puts unsafe?(95)
 
 def not_safe?(speed)
-	
+	if speed <= 40 || speed >= 60 
 end
 	
 
